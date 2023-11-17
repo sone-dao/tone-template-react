ea6127bfe7bca082e1ca177a89a1cb139d0c2ecc
Lon Beshiri <lonbeshiri@gmail.com>
2023-11-17 12:25:20 -0500

    feat: cleanup

--- dist/ServerComponent.d.ts
+++
@@ -1,5 +0,0 @@
-interface IServerComponentProps {
-    text: string;
-}
-export default function ServerComponent({ text }: IServerComponentProps): import("react/jsx-runtime").JSX.Element;
-export {};
