b04b1689dd492a805544a777652ac470ada218e3
Lon Beshiri <lonbeshiri@gmail.com>
2023-11-17 12:24:33 -0500

    refactor: webpack

--- dist/esm/src/index.js
+++
@@ -1,2 +0,0 @@
-export{default as ClientComponent}from"./ClientComponent.js";export{default as ServerComponent}from"./ServerComponent.js";
-//# sourceMappingURL=index.js.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,3 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-
-interface IClientComponentProps {
-    defaultText?: string;
-}
-declare function ClientComponent({ defaultText, }: IClientComponentProps): react_jsx_runtime.JSX.Element;
-
-interface IServerComponentProps {
-    text: string;
-}
-declare function ServerComponent({ text }: IServerComponentProps): react_jsx_runtime.JSX.Element;
-
+import ClientComponent from './ClientComponent';
+import ServerComponent from './ServerComponent';
 export { ClientComponent, ServerComponent };
