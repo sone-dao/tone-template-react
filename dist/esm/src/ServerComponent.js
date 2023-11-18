63147a420ef14de5984bc9ebd83f4c013069d323
Lon Beshiri <lonbeshiri@gmail.com>
2023-11-18 18:18:51 -0500

    feat: rollup cleanup

--- dist/esm/src/ServerComponent.js
+++
@@ -1,2 +0,0 @@
-import{jsx as r}from"react/jsx-runtime";function t(t){var e=t.text;return r("p",{children:e})}export{t as default};
-//# sourceMappingURL=ServerComponent.js.map
--- dist/esm/types/ClientComponent.d.ts
+++
@@ -1,5 +0,0 @@
-interface IClientComponentProps {
-    defaultText?: string;
-}
-export default function ClientComponent({ defaultText, }: IClientComponentProps): import("react/jsx-runtime").JSX.Element;
-export {};
